0315761137bd0717ec4c4792ca712064daf8ef78
Tareq Sifat <mdtareq.me@gmail.com>
2023-06-13 16:21:21 +0600

    create category subCategory migrations

--- db/migrate/20230613101040_create_posts.rb
+++ db/migrate/20230613101040_create_posts.rb
@@ -1,8 +1,16 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.string :user_id, limit:10, default:'NULL'
+      t.string :category_id, limit:10, default:'NULL'
+      t.string :sub_category_id, limit:10, default:'NULL'
+      t.string :title, limit:100, default:'NULL'
+      t.string :slug, limit:100, default:'NULL'
+      t.string :body, default:'NULL'
+      t.string :image, default:'NULL'
       t.timestamps
     end
+
+    add_index :posts, :slug
   end
 end
---
+++ db/migrate/20230613101400_create_sub_categories.rb
@@ -0,0 +1,8 @@
+class CreateSubCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sub_categories do |t|
+
+      t.timestamps
+    end
+  end
+end
